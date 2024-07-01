18cb4ce5eb707c0833bb89192e1590a3a5d93778
Ivan Medvedovskyi <118740539+IvanMedvedovskyi@users.noreply.github.com>
2024-07-01 14:43:54 +0300

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3003/api'
+    baseURL: 'full-stack-pet-project-react-js-node-js-express-js-mongo-db-api.vercel.app'
 })
 
 instance.interceptors.request.use(config => {
